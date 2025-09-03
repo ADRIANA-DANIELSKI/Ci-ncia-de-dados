acdb147a418243a1d0c74e70b2c8d1b428ae2900
ADRIANA-DANIELSKI <adriana.danielski@escola.pr.gov.br>
2025-09-03 19:28:57 +0000

    Aula 6 parte 2

--- aula06.py
+++ aula06.py
@@ -5,5 +5,12 @@ import numpy as np
 np.random.seed(42)
 
 dados = pd.DataFrame({
-    "Orcamento Campanha": np.random.randint(1000, 5000, size=100)
+
+    "Orcamento Campanha": np.random.randint(1000, 5000, size=100),
+    "Visualizacoes Anuncio": np.random.randint(2000, 200000, size=100),
+    "Vendas": np.random.randint(10, 500, size=100),
+    "Cliques": np.random.randint(100, 20000, size=100)
 })
+
+# Visualizar a correlação entre cada variável
+print(dados.corr().round(4))
